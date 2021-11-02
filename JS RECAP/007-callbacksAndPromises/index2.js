bb1b743b63c391fe869cb078a8dbbb4e00d23200
samkazshel <samsheldon779@gmail.com>
2021-11-02 16:23:24 +0000

    created my own version of a promise function

---
+++ JS RECAP/007-callbacksAndPromises/index2.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let promise = new Promise((resolve, reject) => {
+    let a = 5;
+    if (a % 2 == 0) {
+        resolve("That is a even");
+    } else {
+        reject("That is odd");
+    }
+});
+
+promise.then((message) => {
+    console.log(message);
+}).catch((message) => {
+    console.log(message);
+}).then(() => {
+    console.log("This function is looking for whether the number is even or odd")
+})
