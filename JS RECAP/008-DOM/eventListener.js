e395c9dd1bbee9ad1000a678d9a154eceb1ffa91
samkazshel <samsheldon779@gmail.com>
2021-11-03 15:51:57 +0000

    more work on eventlisteners and manipulation the DOM with JS

--- JS RECAP/008-DOM/eventListener.js
+++ JS RECAP/008-DOM/eventListener.js
@@ -42,12 +42,43 @@ let textInput = document.querySelector("#textInput");
 let button5 = document.querySelector("#printButton");
 
 //add an event listener to the button to tell it to do something
-
-button5.addEventListener('click', () => {
+let createNewH2 = () => {
     var newH2 = document.createElement("h2");
     var textIn = document.createTextNode(textInput.value);
     newH2.appendChild(textIn);
     var currentLast = document.getElementById("#textInput");
     document.body.insertBefore(newH2, currentLast);
-});
+}
 
+
+
+
+let cardHolder = document.querySelector("#cardHolder");
+let createNewCard = () => {
+    let newCard = document.createElement('div');
+    let newCardBody = document.createElement('div');
+    let newTitle = document.createElement('h5');
+    let newText = document.createElement('p');
+    let newHyperLink = document.createElement('a');
+
+    newCard.classList = "card";
+    newCard.style = "width: 18rem";
+    newCardBody.classList = "card-body";
+    newTitle.classList = "card-title";
+    newText.classList = "card-text";
+    newHyperLink.classList = "btn btn-primary";
+    newHyperLink.href = "https://google.com";
+
+    newTitle.textContent = "Go to Google"
+    newText.textContent = "GO TO THE OVERLORD";
+    newHyperLink.textContent =  "SEARCH SEARCH SEARCH";
+
+    //append everything
+    newCardBody.appendChild(newTitle);
+    newCardBody.appendChild(newText);
+    newCardBody.appendChild(newHyperLink);
+
+    newCard.appendChild(newCardBody);
+    cardHolder.appendChild(newCard);
+}
+button5.addEventListener('click', createNewCard);
